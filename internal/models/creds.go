ca94202a2dbe1b03a5dbfcf43fcbfdc8ded5fe0c
diyliv <onlogn081@gmail.com>
2023-01-03 21:45:31 +0300

    dto

---
+++ internal/models/creds.go
@@ -0,0 +1,6 @@
+package models
+
+type Creds struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
---
+++ internal/models/cve.go
@@ -0,0 +1,7 @@
+package models
+
+type CVE struct {
+	Name        string `json:"name"`
+	Link        string `json:"link"`
+	Description string `json:"description"`
+}
