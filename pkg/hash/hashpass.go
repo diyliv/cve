bb99ceb9614ca16a6a1bd344d05a7a98152549a8
diyliv <onlogn081@gmail.com>
2023-01-03 21:44:54 +0300

    hash logic

---
+++ pkg/hash/hashpass.go
@@ -0,0 +1,25 @@
+package hash
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPass(pass []byte) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func ComparePass(hashedPass string, plainPass []byte) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPass), plainPass); err != nil {
+		log.Printf("Error while comparing password: %v\n", err)
+		return false
+	}
+
+	return true
+}
