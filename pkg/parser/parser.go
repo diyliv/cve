2b7cf5b1d3d5c2afae27fc1bcd65c46df4cf10cc
diyliv <onlogn081@gmail.com>
2023-01-03 21:44:29 +0300

    parser logic

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -3,17 +3,29 @@ package parser
 import (
 	"fmt"
 
+	"github.com/diyliv/cve/internal/models"
 	"github.com/gocolly/colly/v2"
 )
 
-func ParseCveMitre(search string) {
+func ParseCveMitre(search string) []models.CVE {
 	url := fmt.Sprintf("https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=%s", search)
 
 	c := colly.NewCollector(colly.AllowURLRevisit())
 
-	c.OnHTML("table", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	var data models.CVE
+	var result []models.CVE
+
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(i int, h *colly.HTMLElement) {
+			data.Name = h.ChildText("a")
+			data.Link = "https://cve.mitre.org" + h.ChildAttr("a", "href")
+			data.Description = h.ChildText("td")
+
+			result = append(result, data)
+		})
 	})
 
 	c.Visit(url)
+
+	return result
 }
