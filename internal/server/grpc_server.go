c5a529b4ecc8dcb93881b16b2dbc52b2a62c471f
diyliv <onlogn081@gmail.com>
2023-01-03 21:46:05 +0300

    servers

--- internal/server/grpc_server.go
+++ internal/server/grpc_server.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"database/sql"
+
+	"github.com/diyliv/cve/config"
+	"go.uber.org/zap"
+)
+
+type server struct {
+	logger   *zap.Logger
+	cfg      *config.Config
+	psqlConn *sql.DB
+}
+
+func NewServer(logger *zap.Logger, cfg *config.Config, psqlConn *sql.DB) *server {
+	return &server{
+		logger:   logger,
+		cfg:      cfg,
+		psqlConn: psqlConn,
+	}
+}
+
+func (s *server) StartgRPC() {
+
+}
