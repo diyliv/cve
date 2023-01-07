4880ac457617ea358138c1a4b124e8025a8ae0d4
diyliv <onlogn081@gmail.com>
2023-01-07 03:10:32 +0300

    basic interceptor

--- internal/interceptors/interceptors.go
+++ internal/interceptors/interceptors.go
@@ -1,1 +1,26 @@
 package interceptors
+
+import (
+	"context"
+
+	"github.com/diyliv/cve/config"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+type interceptor struct {
+	logger *zap.Logger
+	cfg    *config.Config
+}
+
+func NewInterceptor(logger *zap.Logger, cfg *config.Config) *interceptor {
+	return &interceptor{
+		logger: logger,
+		cfg:    cfg,
+	}
+}
+
+func (i *interceptor) Logger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+	return nil, nil
+}
