f4ccc2b5877bfee6f5c18103fc98b3cda28b83dc
diyliv <onlogn081@gmail.com>
2023-01-07 16:33:40 +0300

    added loger interceptor logic

--- internal/interceptors/interceptors.go
+++ internal/interceptors/interceptors.go
@@ -2,10 +2,13 @@ package interceptors
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/diyliv/cve/config"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type interceptor struct {
@@ -21,6 +24,14 @@ func NewInterceptor(logger *zap.Logger, cfg *config.Config) *interceptor {
 }
 
 func (i *interceptor) Logger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	start := time.Now()
 
-	return nil, nil
+	md, _ := metadata.FromIncomingContext(ctx)
+	reply, err := handler(ctx, req)
+	i.logger.Info(fmt.Sprintf("Method: %s, Time: %v, Metadata: %v, Err: %v\n",
+		info.FullMethod,
+		time.Since(start),
+		md,
+		err))
+	return reply, err
 }
