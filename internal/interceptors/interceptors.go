c65e1f5ae33662ddb1813ec9f47732c25cc94567
diyliv <onlogn081@gmail.com>
2023-01-08 00:49:20 +0300

    check token interceptor

--- internal/interceptors/interceptors.go
+++ internal/interceptors/interceptors.go
@@ -8,7 +8,9 @@ import (
 	"github.com/diyliv/cve/config"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 type interceptor struct {
@@ -23,7 +25,11 @@ func NewInterceptor(logger *zap.Logger, cfg *config.Config) *interceptor {
 	}
 }
 
-func (i *interceptor) Logger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+func (i *interceptor) Logger(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (resp interface{}, err error) {
 	start := time.Now()
 
 	md, _ := metadata.FromIncomingContext(ctx)
@@ -33,5 +39,31 @@ func (i *interceptor) Logger(ctx context.Context, req interface{}, info *grpc.Un
 		time.Since(start),
 		md,
 		err))
+	return reply, err
+}
+
+func (i *interceptor) CheckToken(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (resp interface{}, err error) {
+	if info.FullMethod == "/cve.CveService/Register" {
+		return handler(ctx, req)
+	}
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "Retrieving metadata in failed")
+	}
+
+	authHeader, ok := md["authorization"]
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "Go to register to get your personal token")
+	}
+
+	token := authHeader[0]
+	fmt.Println(token)
+
+	reply, err := handler(ctx, req)
 	return reply, err
 }
