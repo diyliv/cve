28fc759ff1ea1531fa4ccc0ce8bfb98485b7ec84
diyliv <onlogn081@gmail.com>
2023-01-08 00:50:21 +0300

    logic for login and find vulnerabilities methods

--- internal/cve/delivery/grpc/grpc.go
+++ internal/cve/delivery/grpc/grpc.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"errors"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
@@ -10,7 +11,9 @@ import (
 	"github.com/diyliv/cve/config"
 	"github.com/diyliv/cve/internal/cve"
 	"github.com/diyliv/cve/internal/models"
+	"github.com/diyliv/cve/pkg/errs"
 	"github.com/diyliv/cve/pkg/hash"
+	"github.com/diyliv/cve/pkg/parser"
 	cvepb "github.com/diyliv/cve/proto/cve"
 )
 
@@ -30,27 +33,57 @@ func NewgRPCService(logger *zap.Logger, cfg *config.Config, cveUC cve.PostgresUs
 
 func (gs *grpcservice) Register(ctx context.Context, req *cvepb.RegisterReq) (*cvepb.RegisterResp, error) {
 	data := req.GetUserCreds()
-	var creds models.Creds
 
-	creds.Login = data.Login
-	hashedPassword, err := hash.HashPass([]byte(creds.Password))
+	hashedPassword, err := hash.HashPass([]byte(data.Password))
 	if err != nil {
 		gs.logger.Error("Error while hashing password: " + err.Error())
 		return nil, status.Error(codes.Internal, err.Error())
 	}
-	creds.Password = hashedPassword
 
-	if err := gs.cveUC.Register(ctx, creds); err != nil {
-		return nil, status.Error(codes.AlreadyExists, "This user already exists. Try another login.")
+	if err := gs.cveUC.Register(ctx, models.Creds{Login: data.Login, Password: hashedPassword}); err != nil {
+		if errors.Is(err, errs.ErrAlreadyExists) {
+			return nil, status.Error(codes.AlreadyExists, "This user already exists. Try another login.")
+		}
+		gs.logger.Error("Error while calling cveUC Register: " + err.Error())
 	}
 
 	return &cvepb.RegisterResp{Status: "created"}, status.Error(codes.OK, "")
 }
 
 func (gs *grpcservice) Login(ctx context.Context, req *cvepb.LoginReq) (*cvepb.LoginResp, error) {
-	return nil, nil
+	data := req.GetUserCreds()
+
+	hashedPass, err := gs.cveUC.Login(ctx, data.Login)
+	if err != nil {
+		if errors.Is(err, errs.ErrNotFound) {
+			return nil, status.Error(codes.NotFound, "user not found")
+		}
+		gs.logger.Error("Error while calling cveUC Login: " + err.Error())
+	}
+
+	if !hash.ComparePass(hashedPass, []byte(data.Password)) {
+		return nil, status.Error(codes.Unauthenticated, "incorrect login/password")
+	}
+
+	return &cvepb.LoginResp{Status: "successful login"}, status.Error(codes.OK, "")
 }
 
 func (gs *grpcservice) FindVulnerabilities(ctx context.Context, req *cvepb.FindVulnerabilitiesReq) (*cvepb.FindVulnerabilitiesResp, error) {
-	return nil, nil
+	searchReq := req.GetSearch()
+
+	parseResult := parser.ParseCveMitre(searchReq)
+	if len(parseResult) == 0 {
+		return nil, status.Error(codes.NotFound, errs.ErrNotFound.Error())
+	}
+	var resp []*cvepb.Cve
+
+	for _, val := range parseResult {
+		resp = append(resp, &cvepb.Cve{
+			Name:        val.Name,
+			Link:        val.Link,
+			Description: val.Description,
+		})
+	}
+
+	return &cvepb.FindVulnerabilitiesResp{Result: resp}, status.Error(codes.OK, "")
 }
